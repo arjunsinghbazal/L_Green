3f4b7802c229f064ed3f736d1939cc9a1908d1ad
Arjun Singh <arjunsinghbazal@gmail.com>
2023-09-24 01:52:32 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import AnimatedCursor from "react-animated-cursor"
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const style={ 
+  borderRadius:'50%',border:"2px solid green",
+  backgroundColor: 'lightgreen'
+  }
 root.render(
   <React.StrictMode>
     <App />
+    <AnimatedCursor  innerSize={9}
+      outerSize={29}
+      color="4, 102, 4"
+      zIndex={9999999999}
+      outerAlpha={0.3}
+      innerScale={0.7}
+      outerScale={1.5}
+      outerStyle={style}
+      
+/>
   </React.StrictMode>
 );
 
